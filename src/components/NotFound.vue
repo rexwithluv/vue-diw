d23947a805b01ee77bf2fa7d88598556ec556172
a23aaronar <a23aaronar@iesteis.es>
2024-11-14 10:29:14 +0100

    Añadidos nuevos componentes

---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <NavBar></NavBar>
+    <p>Página no encontrada</p>
+    <img src="../assets/404.png" height="auto" width="700px" alt="">
+    <router-link class="btn btn-primary" :to="{ name: 'inicio' }">Volver</router-link>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+
+export default {
+    name: "NotFound",
+    components: {
+        NavBar
+    }
+}
+</script>
+<style></style>
---
+++ src/components/PaginaInicio.vue
@@ -0,0 +1,17 @@
+<template>
+    <NavBar />
+    <p>Página inicio</p>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+
+export default {
+    name: "PaginaInicio",
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style></style>
