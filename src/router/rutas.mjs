1dd9b48418bb734d07be1002e92d7fcbfb3b9187
a23aaronar <a23aaronar@iesteis.es>
2024-12-05 08:43:17 +0100

    el back sigue a medias

--- src/router/rutas.mjs
+++ src/router/rutas.mjs
@@ -5,7 +5,7 @@ const rutas = express.Router();
 
 rutas.get("/articulos", async (req, res) => {
     try {
-        const articulos = await Articulo.fin(());
+        const articulos = await Articulo.find({});
         res.json(articulos);
     } catch (error) {
         res.status(500).json({ message: error.message });
--- src/server.mjs
+++ src/server.mjs
@@ -1,14 +1,18 @@
 import express from "express";
 import http from "http";
-import morgan from "morgan"
+import morgan from "morgan";
+import mongoose from "mongoose";
+import rutas from "./router/rutas.mjs"
 
 const app = express();
 const server = http.createServer();
 
 app.use(morgan("dev"));
+app.use("/api", rutas);
 app.use(express.json());
+
 app.set("port", process.env.PORT || 5000);
-app.get("/", (req, res) => res.send("Servidor en marcha"));
+app.get("/", (req, res) => res.send("Servidor en funcionamiento."));
 
 server.listen(app.get("port"), () => {
     console.log("Servidor corriendo en el puerto: ", app.get("port"));
