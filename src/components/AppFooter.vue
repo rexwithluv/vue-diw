3474e5bb49b29e1ec25ea9c844c9f3a509bff24f
rexwithluv <aaronaax@gmail.com>
2025-03-12 18:43:43 +0100

    refactor(footer): estilizado el código

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,17 @@
 <template>
   <footer class="bg-primary text-white text-center pb-2 pt-4 mt-4">
     <div class="container">
-      <p class="mb-1">© {{ currentYear }} Mi Empresa. Todos los derechos reservados.</p>
+      <p class="mb-1">
+        © {{ currentYear }} Mi Empresa. Todos los derechos reservados.
+      </p>
       <p>
-        <router-link to="/aviso-legal" class="link d-inline text-white">Aviso Legal</router-link> |
-        <router-link to="/politica-privacidad" class="link d-inline text-white">Política de Privacidad</router-link>
+        <router-link to="/aviso-legal" class="link d-inline text-white">
+          Aviso Legal
+        </router-link>
+        |
+        <router-link to="/politica-privacidad" class="link d-inline text-white">
+          Política de Privacidad
+        </router-link>
       </p>
     </div>
   </footer>
@@ -17,8 +24,8 @@ export default {
   computed: {
     currentYear() {
       return new Date().getFullYear();
-    }
-  }
+    },
+  },
 };
 </script>
 
